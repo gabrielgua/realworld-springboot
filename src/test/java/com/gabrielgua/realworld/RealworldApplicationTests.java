81e7427f5886da46a716a99b6b0a3868a5a25b12
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2024-04-30 11:55:32 -0300

    initial commit

---
+++ src/test/java/com/gabrielgua/realworld/RealworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielgua.realworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
